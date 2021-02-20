f8a86e2e9717ecba325de326de539dad5badface
zeynepidil <77980443+zeynepidil@users.noreply.github.com>
2021-02-20 16:13:03 +0300

    Update HW2.py

--- Homeworks/HW2.py
+++ Homeworks/HW2.py
@@ -1,5 +1,24 @@
 #Explain your work
 
 #Question 1
-for x in range(a):
-	print(a)
+studentsname_list=["Jenny Black","Edward Column","John Newman","Jessica Marley","Bernard Steven"]
+print(studentsname_list)
+name=input("Please enter student name")
+print("Name:{}".format(name))
+y1=input('midtermgrade:')
+y2=input('finalgrade:')
+y3=input('homeworkgrade:')
+average=((float(y1)+float(y2)+float(y3))/3)
+print("Average:{}".format(average))
+
+
+
+gradeslist=[[75,90,85],[60,70,87],[65,76,89],[80,50,55],[85,95,100]]
+grades_dictionary={"Jenny Black":83.33,"Edward Column":72.33,"John Newman":76.67,"Jessica Marley":61.64,"Bernard Steven":93.33}
+print(grades_dictionary)
+print(grades_dictionary["Bernard Steven"])
+
+if(average>77.46):
+    print("Congratulations,you're above average")
+else:
+   print("You're under the average")
