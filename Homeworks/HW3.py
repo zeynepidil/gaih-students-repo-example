ee722762eba808dafd3352652017d00db3abb39d
zeynepidil <77980443+zeynepidil@users.noreply.github.com>
2021-02-20 22:10:09 +0300

    Update HW3.py

--- Homeworks/HW3.py
+++ Homeworks/HW3.py
@@ -1,5 +1,28 @@
 #Explain your work
 
 #Question 1
-for x in range(a):
-	print(a)
+def prime_first(num):
+    num=int(input("Please enter an integer value"))
+    def prime_second(num):  
+        num=int(input("Please enter an integer value"))
+        for i in range(0,1000):
+            for i in range(0,500):  
+                if num>1:       
+                    for i in range(2,num):      
+                        if (num%i)==0:  
+                           print(num,"is not a prime number")      
+                print(i,"times",num//i,"is",num)
+                break       
+            else:   
+                print(num,"is a prime number")
+
+                
+    for i in range(500,1000):
+         if num>1:       
+             for i in range(2,num):      
+                        if (num%i)==0:  
+                           print(num,"is not a prime number")       
+                           print(i,"times",num//i,"is",num)     
+                           break        
+             else:   
+                print(num,"is a prime number")
